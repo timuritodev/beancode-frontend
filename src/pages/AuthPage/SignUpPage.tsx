698babf8513fad8b149ec981bf041b7ddc9b0d65
luciD <sosalnet@yandex.ru>
2023-12-26 21:18:52 +0300

    registraion in progress

--- src/pages/AuthPage/SignUpPage.tsx
+++ src/pages/AuthPage/SignUpPage.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import "./AuthPage.css";
 import { useState } from "react";
 import { useAppDispatch } from "../../../src/services/typeHooks";
 import { signUpUser, setUser } from "../../services/redux/slices/user/user";
@@ -29,77 +30,86 @@ const SignUpPage = () => {
     dispatch(setUser(formData));
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Имя:
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </label>
+    <section className="signup">
+      <div className="signup__container">
+        <h1 className="signup__title">Регистрация</h1>
+        <form className="signup__form" onSubmit={handleSubmit}>
+          <label className="signup__label">Имя</label>
+          <input
+            type="text"
+            name="name"
+            className="signup__input"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Иван"
+            required
+          />
 
-      <label>
-        Фамилия:
-        <input
-          type="text"
-          name="surname"
-          value={formData.surname}
-          onChange={handleChange}
-          required
-        />
-      </label>
+          <label className="signup__label">Фамилия</label>
+          <input
+            type="text"
+            name="surname"
+            className="signup__input"
+            value={formData.surname}
+            onChange={handleChange}
+            placeholder="Иванов"
+            required
+          />
+          {/*
+          <label>
+            Телефон:
+            <input
+              type="text"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              required
+            />
+          </label>
 
-      <label>
-        Телефон:
-        <input
-          type="text"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          required
-        />
-      </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
 
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </label>
+          <label>
+            Адрес:
+            <input
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              required
+            />
+          </label>
 
-      <label>
-        Адрес:
-        <input
-          type="text"
-          name="address"
-          value={formData.address}
-          onChange={handleChange}
-          required
-        />
-      </label>
+          <label>
+            Пароль:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </label> */}
 
-      <label>
-        Пароль:
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </label>
-
-      <button type="submit" onClick={handleSubmit}>
-        Зарегистрироваться
-      </button>
-    </form>
+          <button
+            type="submit"
+            className="signup__button"
+            onClick={handleSubmit}
+          >
+            Зарегистрироваться
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 export default SignUpPage;
