9ecbb9367e712b1edca4bef5d8d98b2c6b72b311
luciD <sosalnet@yandex.ru>
2023-12-31 14:12:26 +0300

    added count on product-card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,22 +1,23 @@
+import { FC } from "react";
 import "./ProductCard.css";
-// import { useAppSelector } from "../../services/typeHooks";
-import { IProduct } from "../../types/Product.types";
 import img from "../../images/product.jpg";
+import { IProduct } from "../../types/Product.types";
 
-export const ProductCard = ({ data }: { data: IProduct }) => {
-  // const cart = useAppSelector((state) => state.cart.cart);
+interface ProductCardProps {
+  data: IProduct;
+  count: number;
+}
 
-  // const handleClickButton = () => {
-  //     navigate('/cart-page')
-  // }
+export const ProductCard: FC<ProductCardProps> = ({ data, count }) => {
   return (
     <div className="product-card">
-      <img className="product-card__img" src={img} />
+      <img className="product-card__img" src={img} alt={data.title} />
       <div className="product-card__text-container">
         <h3 className="product-card__title">{data.title}</h3>
         <p className="product-card__text">{data.weight}</p>
       </div>
       <p className="product-card__price">{data.price}</p>
+      {count > 1 && <p className="product-card__count">Count: {count}</p>}
     </div>
   );
 };
--- src/components/ProductCard/ProductCardList.tsx
+++ src/components/ProductCard/ProductCardList.tsx
@@ -1,13 +1,44 @@
-import "./ProductCard.css";
-import { IProductsProp } from "../../types/Product.types";
+import { IProduct } from "../../types/Product.types";
 import { FC } from "react";
 import { ProductCard } from "./ProductCard";
 
-export const ProductCardList: FC<IProductsProp> = ({ data }) => {
+interface ProductCardListProps {
+  data: IProduct[];
+}
+
+export const ProductCardList: FC<ProductCardListProps> = ({ data }) => {
+
+  const countProducts = (products: IProduct[]) => {
+    const productCount: Record<number, number> = {};
+    products.forEach((product) => {
+      const productId = product.id;
+      productCount[productId] = (productCount[productId] || 0) + 1;
+    });
+    return productCount;
+  };
+
+  const productCounts = countProducts(data);
+
+  const uniqueData = Array.from(new Set(data.map((item) => item.id))).map(
+    (id) => {
+      const product = data.find((item) => item.id === id);
+      return {
+        ...product,
+        count: productCounts[id],
+      };
+    }
+  );
+
   return (
     <div className="product-card-list">
-      {data.length !== 0 &&
-        data.map((item) => <ProductCard key={item.id} data={item} />)}
+      {uniqueData.length !== 0 &&
+        uniqueData.map((item) => (
+          <ProductCard
+            key={item.id}
+            data={item as IProduct}
+            count={item.count}
+          />
+        ))}
     </div>
   );
 };
