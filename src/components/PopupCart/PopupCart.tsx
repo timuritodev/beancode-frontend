373c059bc63da465e8f054e706aa72eaddcb7095
luciD <sosalnet@yandex.ru>
2023-12-31 03:40:56 +0300

    almost done with cart-popup

--- src/components/PopupCart/PopupCart.tsx
+++ src/components/PopupCart/PopupCart.tsx
@@ -1,27 +1,58 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useAppSelector } from '../../services/typeHooks';
-import { FC } from 'react';
-import './PopupCart.css'
-import { ProductCardList } from '../ProductCard/ProductCardList';
+import { useAppSelector } from "../../services/typeHooks";
+import { FC } from "react";
+import "./PopupCart.css";
+import { ProductCardList } from "../ProductCard/ProductCardList";
 
 interface PopupCartProps {
-    isPopupOpen: boolean;
-    switchPopupTrailer: () => void;
+  isPopupOpen: boolean;
+  switchPopupTrailer: () => void;
 }
 
-
 export const PopupCart: FC<PopupCartProps> = ({
-    isPopupOpen,
-    switchPopupTrailer,
+  isPopupOpen,
+  switchPopupTrailer,
 }) => {
-    const cartproducts = useAppSelector((state) => state.cart.cart);
+  const cartproducts = useAppSelector((state) => state.cart.cart);
 
-    return (
-        <div className={`popupTrailer ${isPopupOpen ? 'popupTrailer_opened' : ''}`}>
-            <div className="popupTrailer__content">
-                <ProductCardList data={cartproducts} />
-            </div>
+  let sum = 2;
+
+  cartproducts.forEach((product) => {
+    sum += parseInt(product.price, 10);
+  });
+
+  return (
+    <div className={`popup-cart ${isPopupOpen ? "popup-cart_opened" : ""}`}>
+      <div className="popup-cart__content">
+        <h1 className="popup-cart__title">Ваша корзина</h1>
+        <p className="popup-cart__text">
+          В вашей корзине {cartproducts.length} товаров
+        </p>
+        <ProductCardList data={cartproducts} />
+        <div className="popup-cart__info__container">
+          <button className="popup-cart__button-delete" type="button">
+            Удалить все
+          </button>
+          <p className="popup-cart__text__sum">Всего: {sum} ₽</p>
         </div>
-    );
+        <div className="popup-cart__button__container">
+          <button
+            className="popup-cart__button-continue"
+            type="button"
+            onClick={switchPopupTrailer}
+          >
+            Продолжить покупки
+          </button>
+          <button className="popup-cart__button-order" type="button">
+            Оформить заказ
+          </button>
+        </div>
+      </div>
+      <button
+        className="popup-cart__close"
+        type="button"
+        onClick={switchPopupTrailer}
+      />
+    </div>
+  );
 };
-
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,20 +4,19 @@ import { IProduct } from "../../types/Product.types";
 import img from "../../images/product.jpg";
 
 export const ProductCard = ({ data }: { data: IProduct }) => {
-    // const cart = useAppSelector((state) => state.cart.cart);
+  // const cart = useAppSelector((state) => state.cart.cart);
 
-    // const handleClickButton = () => {
-    //     navigate('/cart-page')
-    // }
-    return (
-        <div className="product-card">
-            <div className="product-card__container">
-                <img className="product-card__img" src={img} />
-                <h3 className="product-card__title">{data.title}</h3>
-                <p className="product-card__text">{data.weight}</p>
-                <p className="product-card__price">{data.price}</p>
-            </div>
-        </div>
-    );
+  // const handleClickButton = () => {
+  //     navigate('/cart-page')
+  // }
+  return (
+    <div className="product-card">
+      <img className="product-card__img" src={img} />
+      <div className="product-card__text-container">
+        <h3 className="product-card__title">{data.title}</h3>
+        <p className="product-card__text">{data.weight}</p>
+      </div>
+      <p className="product-card__price">{data.price}</p>
+    </div>
+  );
 };
-
