12599065cfc2a42c32f6d71a69a90c274bf3c3f0
luciD <sosalnet@yandex.ru>
2024-01-04 15:48:47 +0300

    update css in productsSlider

--- src/components/ProductsSlider/ProductsSlider.tsx
+++ src/components/ProductsSlider/ProductsSlider.tsx
@@ -6,11 +6,6 @@ import "slick-carousel/slick/slick-theme.css";
 import { FC } from "react";
 import { IProductsProp } from "../../types/Product.types";
 import { SmallProduct } from "../SmallProduct/SmallProduct";
-// import { CustomNextArrow, CustomPrevArrow } from '../Arrows/Arrows';
-// import arrow_next from "../../images/arrow-next.svg";
-// import arrow_prev from "../../images/arrow-prev.svg";
-import arrow_next_2 from "../../images/arrow_next_2.svg";
-import arrow_prev_2 from "../../images/arrow_prev_2.svg";
 
 export const ProductsSlider: FC<IProductsProp> = ({ data }) => {
   const settings = {
@@ -20,12 +15,6 @@ export const ProductsSlider: FC<IProductsProp> = ({ data }) => {
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: true,
-    // prevArrow: <CustomPrevArrow />,
-    // nextArrow: <CustomNextArrow />,
-    prevArrow: <img className="slick-prev" src={arrow_prev_2} alt="Previous" />,
-    nextArrow: <img className="slick-next" src={arrow_next_2} alt="Next" />,
-    // prevArrow: <SlickArrowLeft />,
-    // nextArrow: <SlickArrowRight {...} />,
     responsive: [
       {
         breakpoint: 1320,
