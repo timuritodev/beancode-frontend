00822ec33fa6bf3c91f65920c85cab3ed8520173
luciD <sosalnet@yandex.ru>
2024-02-14 02:46:30 +0300

    added mobile for profile-page and auth-pages

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import "./CustomButton.css";
-import { useLocation } from "react-router-dom";
 
 export interface ICustomButton {
 	buttonText: string;
@@ -17,31 +16,10 @@ export const CustomButton: FC<ICustomButton> = ({
   disabled,
   className,
 }) => {
-  const location = useLocation();
+
   return (
     <button
-      className={
-        className
-          ? className
-          : `${
-              buttonText !== "Сохранить" && buttonText !== "Войти"
-                ? `${
-                    location.pathname === "/sign-in" ||
-                    location.pathname === "/sign-up"
-                      ? "button button_type_toMain-sign-in"
-                      : "button button_type_toMain"
-                  }`
-                : `${
-                    location.pathname === "/preferences"
-                      ? "button button_type_preferences"
-                      : `${
-                          location.pathname === "/profile"
-                            ? "button button_type_profile"
-                            : "button"
-                        }`
-                  }`
-            }`
-      }
+      className="button"
       disabled={disabled}
       type={type}
       onClick={handleButtonClick}
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,9 @@
 import "./ProfilePage.css";
 import exit_button from "../../images/exit-button.svg";
 import cart from "../../images/shopping-cart.svg";
-// import cart_active from "../../images/shopping-cart_acitve.svg";
+import cart_active from "../../images/shopping-cart_acitve.svg";
 import person from "../../images/person.svg";
-// import person_active from "../../images/person_active.svg";
+import person_active from "../../images/person_active.svg";
 import { selectUser, signOut } from "../../services/redux/slices/user/user";
 import { useAppDispatch, useAppSelector } from "../../services/typeHooks";
 import { useState } from "react";
@@ -12,10 +12,12 @@ import { ProfileInputs } from "../../components/ProfileInputs/ProfileInputs";
 import { orders } from "../../utils/constants";
 import { OrderList } from "../../components/Order/OrderList";
 import { resetOrders } from "../../services/redux/slices/order/order";
+import { useResize } from "../../hooks/useResize";
 
 export const ProfilePage = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
+  const { width } = useResize();
 
   const [isOrderVisible, setIsOrderVisible] = useState(false);
   const [isAccountVisible, setIsAccountVisible] = useState(true);
@@ -30,6 +32,7 @@ export const ProfilePage = () => {
     setIsOrderVisible(false);
   };
 
+  console.log(isAccountVisible)
   return (
     <section className="profile">
       <div className="profile__container">
@@ -38,15 +41,56 @@ export const ProfilePage = () => {
             className="button__profile"
             onClick={handleAccountButtonClick}
           >
-            <img className="button__profile__img" src={person} />
-            <span className="button__profile__text">Личные данные</span>
+            <img className="button__profile__img" src={isAccountVisible ? person_active : person} />
+            <span
+              className={`button__profile__text ${
+                isAccountVisible === true ? "button__profile__text_active" : ""
+              }`}
+            >
+              Личные данные
+            </span>
           </button>
           <button className="button__profile" onClick={handleOrderButtonClick}>
-            <img className="button__profile__img" src={cart} />
-            <span className="button__profile__text">Заказы</span>
+            <img className="button__profile__img" src={isOrderVisible ? cart_active : cart} />
+            <span
+              className={`button__profile__text ${
+                isOrderVisible === true ? "button__profile__text_active" : ""
+              }`}
+            >
+              Заказы
+            </span>
           </button>
+          {width > 767 && (
+            <button
+              className="button__profile"
+              onClick={() => {
+                dispatch(signOut());
+                dispatch(resetCart());
+                dispatch(resetOrders());
+              }}
+            >
+              <img className="button__profile__img" src={exit_button} />
+              <span className="button__profile__text">Выйти</span>
+            </button>
+          )}
+        </div>
+        {isAccountVisible && user.token !== "" ? (
+          <ProfileInputs />
+        ) : (
+          user.token === "" && (
+            <p className="profile__text">Нужно Зарегистрироваться</p>
+          )
+        )}
+        {isOrderVisible && orders.length !== 0 && user.token !== "" ? (
+          <OrderList data={orders} />
+        ) : (
+          isOrderVisible &&
+          orders.length === 0 &&
+          user.token !== "" && <p className="profile__text">Нет заказов</p>
+        )}
+        {width < 767 && (
           <button
-            className="button__profile"
+            className="button__profile button__profile_exit"
             onClick={() => {
               dispatch(signOut());
               dispatch(resetCart());
@@ -56,16 +100,6 @@ export const ProfilePage = () => {
             <img className="button__profile__img" src={exit_button} />
             <span className="button__profile__text">Выйти</span>
           </button>
-        </div>
-        {isAccountVisible && user.token !== "" ? (
-          <ProfileInputs />
-        ) : (
-          user.token === "" && <p className="profile__text">Нужно Зарегистрироваться</p>
-        )}
-        {isOrderVisible && orders.length !== 0 && user.token !== "" ? (
-          <OrderList data={orders} />
-        ) : (
-          isOrderVisible && orders.length === 0 && user.token !== "" && <p className="profile__text">Нет заказов</p>
         )}
       </div>
     </section>
